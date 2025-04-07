c922c65df3b56086d988e9e86d704a13fd75ac48
Matuwamai <wamaimatu@gmail.com>
2025-04-07 12:42:58 +0300

    Setup project

---
+++ frontend/src/main.tsx
@@ -0,0 +1,14 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import { BrowserRouter as Router ,Routes, Route } from 'react-router-dom'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <Router>
+      <Routes>
+        <Route path='' element={<></>} />
+      </Routes>
+    </Router>
+  </StrictMode>,
+)
